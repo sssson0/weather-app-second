a839bfd3f2cd8f6aac54af984f589c1bb1f39eb0
sssson0 <s20023094@gmail.com>
2024-08-19 19:34:24 +0900

    화씨 소수점 수정

--- src/conponent/WeatherBox.js
+++ src/conponent/WeatherBox.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function WeatherBox({weather}) {
     console.log(weather)
+    const FF = weather?.main.temp * 9/5 + 32
   return (
     <div className='weather-box'>
         <div>{weather?.name}</div>
-        <h2>{weather?.main.temp} °C / {weather?.main.temp * 9/5 + 32} °F</h2>
+        <h2>{weather?.main.temp} °C / {FF.toFixed(1)}°F</h2>
         <h3>{weather?.weather[0].description}</h3>
     </div>
   );
